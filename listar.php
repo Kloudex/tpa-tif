00584e49f795d9777eba9d6f69f894549fb5f492
Pablo Pagella <pablopagella1996@gmail.com>
2022-10-17 22:52:08 -0300

    listad solo nombres de dictadores y listar para el backend

--- listar.php
+++ listar.php
@@ -6,13 +6,13 @@
 
 <body>
     <?php
-$conexion mysqli_connect("localhost","root","","tpa-tif") or die ("Error connecting");
-$registros mysqli_query($conexion,"select codigo,nombre,institucion,status from dictadores") or die ("Error en el select:".mysqli_query($conexion));
-while ($req=mysqli_fetch_array($registros)) {
-    echo "Codigo:". $reg['codigo'] . "<br>";
-    echo "Nombre:" . $reg['nombre'] . "<br>";
-    echo "Institución:" . $reg['institucion'] . "<br>";
-    echo "Status:" . $reg['status'] . "<br>";
+    $conexion= mysqli_connect("localhost","root","","tpa-tif") or die("problema con la conexion");
+$registros= mysqli_query($conexion,"select codigo,nombre,institucion,status from dictadores") or die ("Error en el select:".mysqli_query($conexion));
+while ($reg=mysqli_fetch_array($registros)) {
+    echo "Codigo:".$reg['codigo']."<br>";
+    echo "Nombre:".$reg['nombre']."<br>";
+    echo "Institución:".$reg['institucion']."<br>";
+    echo "Status:".$reg['status']."<br>";
     echo "<br>";
     echo "<br>";
 }
