71386a7e201da6595be60e8f753494b2e3d33a9c
Esteban Bonepelche <e.bonepelche@gmail.com>
2022-10-19 21:59:20 -0300

    Help

--- js/db.js
+++ js/db.js
@@ -1,7 +1,6 @@
 const formulario= document.getElementById('formulario');
-
-formulario.addEventListener('submit', (e) => {
-   e.preventDefault(); 
+formulario.addEventListener('button', (e) => {
+    alert();
    fetch('https://sheet.best/api/sheets/0baed9c3-8992-4a8e-96e2-08bee020b82b',{
     method: 'POST',
     mode: 'cors',
@@ -10,8 +9,7 @@ formulario.addEventListener('submit', (e) => {
     },
     body: JSON.stringify({
         "Institución": formulario.nombre.value,
-        "NombreAgregado": formulario.nombre.value,
-        "NombreEliminado": 'Ejemplo de Censura'
+        "NombreAgregado": formulario.nombre.value
     })
    });
 });
